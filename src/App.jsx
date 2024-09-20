23cf171cca8d59e112dab24cb60e8ff89d9b1008
TetyanaLugova <148479156+TetyanaLugova@users.noreply.github.com>
2024-09-20 08:04:13 +0300

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import css from "./App.module.css"
 
 function App() {
   return(
   <BrowserRouter>
+  <div className={css.img}>
+  <Header/>
+  <Hero/>
+  </div>
+    
+ 
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route path="/" element={<div />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import css from "./Hero.module.css"
+
+export default function Hero() {
+    return(
+        <div className={css.hero}>
+        <h1 className={css.heroTitle}>Виплети радість</h1>
+        <p className={css.heroText}>Майстеркласи з в'язання гачком іграшок</p>
+        <p className={css.heroBtn}>Майстеркласи</p>
+      </div>
+    )
+    
+}
