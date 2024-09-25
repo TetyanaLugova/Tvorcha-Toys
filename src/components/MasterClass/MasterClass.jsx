a9c2f188c025a9337b4868918409aab7f1ed85f3
TetyanaLugova <148479156+TetyanaLugova@users.noreply.github.com>
2024-09-25 11:36:37 +0300

    MC

--- src/components/MasterClass/MasterClass.jsx
+++ src/components/MasterClass/MasterClass.jsx
@@ -11,80 +11,160 @@ export default function MasterClass() {
             className={css.img}
             src="/public/img/Корівка_Му-min.jpg"
             alt="Корівка"
-            width="284px"
-            height="260px"
+            width="200px"
+            height="180px"
             loading="lazy"
           />
+          <div className={css.wrapInfo}>
+            <div className={css.wrapText}>
+              <p className={css.textMc}>МК "Корівка Му"</p>
+              <p className={css.textMc}>150 ₴</p>
+            </div>
+            <div className={css.wrapBtn}>
+              <button className={css.btn}>Детальніше</button>
+              <button className={css.btn}>Придбати</button>
+            </div>
+          </div>
         </li>
         <li className={css.item}>
           <img
             className={css.img}
             src="/public/img/Кроличка-min.jpg"
             alt="Зайчик"
-            width="284px"
-            height="260px"
+            width="200px"
+            height="180px"
             loading="lazy"
           />
+          <div className={css.wrapInfo}>
+            <div className={css.wrapText}>
+              <p className={css.textMc}>МК "Кроличка"</p>
+              <p className={css.textMc}>150 ₴</p>
+            </div>
+            <div className={css.wrapBtn}>
+              <button className={css.btn}>Детальніше</button>
+              <button className={css.btn}>Придбати</button>
+            </div>
+          </div>
         </li>
         <li className={css.item}>
           <img
             className={css.img}
             src="/public/img/Корівка_з_бомбонами-min.jpg"
             alt="Корівка з бомбонами"
-            width="284px"
-            height="260px"
+            width="200px"
+            height="180px"
             loading="lazy"
           />
+          <div className={css.wrapInfo}>
+            <div className={css.wrapText}>
+              <p className={css.textMc}>МК "Корівка з бомбонами"</p>
+              <p className={css.textMc}>150 ₴</p>
+            </div>
+            <div className={css.wrapBtn}>
+              <button className={css.btn}>Детальніше</button>
+              <button className={css.btn}>Придбати</button>
+            </div>
+          </div>
         </li>
         <li className={css.item}>
           <img
             className={css.img}
             src="/public/img/Пряничка-min.jpg"
             alt="Пряник"
-            width="284px"
-            height="260px"
+            width="200px"
+            height="180px"
             loading="lazy"
           />
+          <div className={css.wrapInfo}>
+            <div className={css.wrapText}>
+              <p className={css.textMc}>МК "Пряничка"</p>
+              <p className={css.textMc}>150 ₴</p>
+            </div>
+            <div className={css.wrapBtn}>
+              <button className={css.btn}>Детальніше</button>
+              <button className={css.btn}>Придбати</button>
+            </div>
+          </div>
         </li>
         <li className={css.item}>
           <img
             className={css.img}
             src="/public/img/Панда-min.jpg"
             alt="Панда"
-            width="284px"
-            height="260px"
+            width="200px"
+            height="180px"
             loading="lazy"
           />
+          <div className={css.wrapInfo}>
+            <div className={css.wrapText}>
+              <p className={css.textMc}>МК "Панда"</p>
+              <p className={css.textMc}>150 ₴</p>
+            </div>
+            <div className={css.wrapBtn}>
+              <button className={css.btn}>Детальніше</button>
+              <button className={css.btn}>Придбати</button>
+            </div>
+          </div>
         </li>
         <li className={css.item}>
           <img
             className={css.img}
             src="/public/img/Курчатко-min.JPG"
             alt="Курча"
-            width="284px"
-            height="260px"
+            width="200px"
+            height="180px"
             loading="lazy"
           />
+          <div className={css.wrapInfo}>
+            <div className={css.wrapText}>
+              <p className={css.textMc}>МК "Курчатко"</p>
+              <p className={css.textMc}>150 ₴</p>
+            </div>
+            <div className={css.wrapBtn}>
+              <button className={css.btn}>Детальніше</button>
+              <button className={css.btn}>Придбати</button>
+            </div>
+          </div>
         </li>
         <li className={css.item}>
           <img
             className={css.img}
             src="/public/img/Мишка_Мія-min.jpg"
             alt="Мишка"
-            width="284px"
-            height="260px"
+            width="200px"
+            height="180px"
             loading="lazy"
           />
+          <div className={css.wrapInfo}>
+            <div className={css.wrapText}>
+              <p className={css.textMc}>МК "Мишка Мія"</p>
+              <p className={css.textMc}>150 ₴</p>
+            </div>
+            <div className={css.wrapBtn}>
+              <button className={css.btn}>Детальніше</button>
+              <button className={css.btn}>Придбати</button>
+            </div>
+          </div>
         </li>
         <li className={css.item}>
           <img
             className={css.img}
             src="/public/img/Зефірка-min.jpg"
             alt="Єдиноріжка"
-            width="284px"
-            height="260px"
+            width="200px"
+            height="180px"
             loading="lazy"
           />
+          <div className={css.wrapInfo}>
+            <div className={css.wrapText}>
+              <p className={css.textMc}>МК "Зефірка"</p>
+              <p className={css.textMc}>150 ₴</p>
+            </div>
+            <div className={css.wrapBtn}>
+              <button className={css.btn}>Детальніше</button>
+              <button className={css.btn}>Придбати</button>
+            </div>
+          </div>
         </li>
       </ul>
     </div>
