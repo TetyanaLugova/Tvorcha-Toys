1c834774971852be93d72a483b08d7fad17a97b8
TetyanaLugova <148479156+TetyanaLugova@users.noreply.github.com>
2024-10-04 17:25:09 +0300

    fix-footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,33 +2,43 @@ import css from './Footer.module.css';
 import Logo from '../Logo/Logo';
 import { FaTelegram } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa6';
+import { IoShieldCheckmark } from 'react-icons/io5';
 
 export default function Footer() {
   return (
     <div className={css.container}>
-      <div className={css.wrapTitle}>
-        <p className={css.logo}>
-          tvorcha.<span className={css.span}>toys</span>
-        </p>
-        <p>ФОП Семенюк Валентина Іллівна</p>
-        <p>ЄДРПОУ 11111111</p>
-      </div>
-      <div>
-        <p>Контакти</p>
-        <div>
-          <p>Email</p>
-          <a href="valentina.shveda54@gmail.com">
-            valentina.shveda54@gmail.com
-          </a>
+      <div className={css.wrapper}>
+        <div className={css.wrapTitle}>
+          <p className={css.logo}>
+            tvorcha.<span className={css.span}>toys</span>
+          </p>
+          <div className={css.wrapInfo}>
+            <p className={css.textInfo}>Працюю офіційно</p>
+            <IoShieldCheckmark className={css.iconInfo} />
+          </div>
+          <p className={css.text}> ФОП Семенюк Валентина Іллівна</p>
+          <p className={css.text}>ЄДРПОУ 11111111</p>
         </div>
-        <div>
-          <a href="valentina.shveda54@gmail.com">
-            <FaTelegram className={css.icon} />
-          </a>
-          <a href="valentina.shveda54@gmail.com">
-            <FaInstagram className={css.icon} />
-          </a>
+        <div className={css.wrapContact}>
+          <p className={css.textInfo}>Контакти</p>
+          <div>
+            <a className={css.email} href="valentina.shveda54@gmail.com">
+              valentina.shveda54@gmail.com
+            </a>
+          </div>
+          <div className={css.wrapIcon}>
+            <a href="https://t.me/valentinashveda">
+              <FaTelegram className={css.icon} />
+            </a>
+            <a href="https://www.instagram.com/tvorcha.toys?igsh=Mmtocm1haG50Njdh&utm_source=qr">
+              <FaInstagram className={css.icon} />
+            </a>
+          </div>
         </div>
+        {/* <div className={css.wrapAutor}>
+        <p>Сайт створила</p>
+        <a href="Lugovatanya1503@gmail.com">Lugovatanya1503@gmail.com</a>
+      </div> */}
       </div>
     </div>
   );
